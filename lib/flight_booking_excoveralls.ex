5230a8a3b29b1f16b10601166b94a17dbd9c3269
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2022-11-12 21:05:46 -0300

    Feat - created initial config

---
+++ lib/flight_booking_excoveralls.ex
@@ -0,0 +1,18 @@
+defmodule FlightBookingExcoveralls do
+  @moduledoc """
+  Documentation for `FlightBookingExcoveralls`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FlightBookingExcoveralls.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/flight_booking_excoveralls_test.exs
@@ -0,0 +1,8 @@
+defmodule FlightBookingExcoverallsTest do
+  use ExUnit.Case
+  doctest FlightBookingExcoveralls
+
+  test "greets the world" do
+    assert FlightBookingExcoveralls.hello() == :world
+  end
+end
